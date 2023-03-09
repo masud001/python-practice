20c02e005e7be12efb16d46d8e9a239231c8f94e
masud001 <madhnagar@gmail.com>
2023-03-10 01:51:55 +0600

    variable and hello world program

---
+++ Hello.py
@@ -0,0 +1,7 @@
+# this is the hello world program.
+"""
+this is the multiline comments.
+
+"""
+
+print( "Hello world" )
---
+++ variable-and-data-type.py
@@ -0,0 +1,4 @@
+variable = "Variable and data type..."
+_variableName = "this is variable name."
+print(variable)
+print(_variableName)
