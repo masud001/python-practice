9b939c167e2d8622dee079500c2ccc7661fe8723
masud001 <madhnagar@gmail.com>
2023-03-14 12:01:55 +0600

    variable swap function works now

--- variable-and-data-type.py
+++ variable-and-data-type.py
@@ -1,16 +1,1 @@
-variable = "Variable and data type..."
-_variableName = "this is variable name."
-print(variable)
-print(_variableName)
-age = 20
-print(age,id(age))
-age = 30
-
-print(age, id(age))
-# name,age = map(str, input().split(" "))
-name = str(input())
-age = str(input())
-
-print("The name of the person is "+name+" and the age is "+age)
-
 
