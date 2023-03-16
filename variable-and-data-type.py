ff1544ed297daa21f82d532d5daa989c37e15239
masud001 <madhnagar@gmail.com>
2023-03-17 00:04:11 +0600

    sum of al even number

--- variable-and-data-type.py
+++ variable-and-data-type.py
@@ -1,2 +1,20 @@
 # identity operator
 
+# s = input()
+# e = input()
+# w = input()
+# 36
+# 618
+# 78
+import math
+
+for num in range(36, 618, 78):
+    f_value = (num - 32) * 5 / 9
+    if(f_value > 0):
+        print(num, math.floor(f_value))
+    else:
+        print(num, math.ceil(f_value))
+
+
+
+
